efc7b3c65c8051bbd1d4696904dddefff541abc7
GedSid <joaquin.g.ulloa@gmail.com>
2016-06-24 22:06:27 -0300

    esta terminado la parte de ROS del serie, pero no anda

--- Proyecto/catkin_ws/src/cros/emisorVelocidad.py
+++ Proyecto/catkin_ws/src/cros/emisorVelocidad.py
@@ -1,42 +1,43 @@
 #!/usr/bin/env python
 
-# Nodo para ROS que recibe un mensaje geometrico y en funsion genera un PWM
-# Usa pi-blaster, asi que necesita tenerlo instalado y corriendo
+# Nodo para ROS que recibe un mensaje geometrico y en funsion de estos adecua el valor a los motores del dreamster 
+# Envia los nuevos valores por serial al dreamster 
 
-import rospy, sys, traceback
+import rospy, serial, time
 from geometry_msgs.msg import Twist
 
-import numpy as np
-from math import atan2, pi
-
-base_diameter = 0.20 #In meters
-wheel_radius = 0.04  #In meters
-
+base_diameter = 0.105 #In meters
+wheel_radius = 0.038  #In meters
+header = 255 #Defino el header para indicarle al dreamster que inicia la comunicacion
 ###############################################################################
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", str(data.linear))	# escribe: I heard "lo que recibio"
 
 ###############################################################################
 def receiver:
-	rospy.init_node('Velocity', anonymous=True)			# se inicializa el nodo con nombre "nodo3"
+	rospy.init_node('Velocity_serial', anonymous=True)			# se inicializa el nodo con nombre "Velocity_serial"
 	rospy.Subscriber("chatter_vel", Twist, callback)	# se subscribe al canal "chatter_vel" y tiene que recibir un String y llama a callback
-
+	
 
 # Se calcula la velocidad de cada rueda indep, con la posicion lineal y anguular
-def get_wheels_vel(linear_vel_x, ang_vel_z):
-    if linear_vel_x < 0:
-        ang_vel_z = -ang_vel_z
-    aux_r_wheel_vel = 0.5*(((2*linear_vel_x)/wheel_radius) - ((ang_vel_z*base_diameter)/wheel_radius))
-    aux_l_wheel_vel = 0.5*(((2*linear_vel_x)/wheel_radius) + ((ang_vel_z*base_diameter)/wheel_radius))
-    l_wheel_vel = 2.5*aux_l_wheel_vel
-    r_wheel_vel = 2.5*aux_r_wheel_vel
+# Lo que recibo lo leo como data y si quiero velocidad deberia poner data.linear 
+def get_wheels_vel(data.linear_vel_x, data.ang_vel_z):
+    if data.linear_vel_x < 0:
+        data.ang_vel_z = -data.ang_vel_z
+    aux_r_wheel_vel = 0.5*(((2*data.linear_vel_x)/wheel_radius) - ((data.ang_vel_z*base_diameter)/wheel_radius))
+    aux_l_wheel_vel = 0.5*(((2*data.linear_vel_x)/wheel_radius) + ((data.ang_vel_z*base_diameter)/wheel_radius))
+    l_wheel_vel = int(2.5*aux_l_wheel_vel)
+    r_wheel_vel = int(2.5*aux_r_wheel_vel)
     return l_wheel_vel, r_wheel_vel
 ###############################################################################
-def EL QUE ENVIA POR PUERTO SERIE 
+def Serial_send:
     import serial
-    ser = serial.Serial(0)  # open first serial port
-    print ser.portstr       # check which port was really used
-    ser.write("hello")      # write a string
+    ser = serial.Serial()  # open first serial port
+    ser.port = "/dev/ttyACM0" # indica el puerto del dreamster
+    ser.open()
+    ser.write(header)
+    ser.write(l_wheel_vel)
+    ser.write(r_wheel_vel)      # escribe en el puerto los dos valores de velocidad
     ser.close()             # close port
 ###############################################################################
 def Velocity_control_node():
@@ -45,29 +46,16 @@ def Velocity_control_node():
 
     while not rospy.is_shutdown():
 		receiver()			# Recibe info de otro nodo
-		linear_vel,angular_vel=get_wheels_vel() #genera los nuevos valores para las velocidades 
-		ENVIO POR PUERTO SERIE LO QUE RECIBI			# Carga el valor del PWM
+		data.linear_vel,data.angular_vel=get_wheels_vel() #genera los nuevos valores para las velocidades 
+		Serial_send()			# Envia por serie al dreamster
     rospy.spin()		# evita que se cierre el nodo
     file.close()		# se cierra el archivo al salir del nodo
 
 if __name__ == '__main__':
 	#try:
-		PWM_control_node()
+		#PWM_control_node()
     #except rospy.ROSInterruptException:
     #    stop_motors()
     #    PWM.cleanup()
 
 
-# la formula para calcular la velocidad usada en oects_detector es vr=(2v+wL)/2R y vl=(2v-wL)/2R
-
-#https://github.com/mwilliams03/pi-blaster
-#http://ozzmaker.com/software-pwm-on-a-raspberry-pi/
-#Channel number    GPIO number   Pin in P1 header
-#      0               4             P1-7
-#      1              17             P1-11
-#      2              18             P1-12
-#      3              21             P1-13
-#      4              22             P1-15
-#      5              23             P1-16
-#      6              24             P1-18
-#      7              25             P1-22
