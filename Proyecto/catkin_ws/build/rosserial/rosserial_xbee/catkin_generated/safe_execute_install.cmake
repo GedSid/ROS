ab73f93c15db9e722eb24868843ba84c8fa40d46
GedSid <joaquin.g.ulloa@gmail.com>
2016-07-01 22:18:16 -0300

    movemos el robot por puerto serie, pero hace cualquier cosa

--- Proyecto/catkin_ws/build/rosserial/rosserial_xbee/catkin_generated/safe_execute_install.cmake
+++
@@ -1,5 +0,0 @@
-execute_process(COMMAND "/home/alumno/Escritorio/ros2/Proyecto/catkin_ws/build/rosserial/rosserial_xbee/catkin_generated/python_distutils_install.sh" RESULT_VARIABLE res)
-
-if(NOT res EQUAL 0)
-  message(FATAL_ERROR "execute_process(/home/alumno/Escritorio/ros2/Proyecto/catkin_ws/build/rosserial/rosserial_xbee/catkin_generated/python_distutils_install.sh) returned error code ")
-endif()
--- Proyecto/catkin_ws/devel/share/rosserial_tivac/cmake/rosserial_tivac-extras.cmake
+++
@@ -1,3 +0,0 @@
-cmake_minimum_required(VERSION 2.8.3)
-
-set(ROSSERIAL_TIVAC_TOOLCHAIN "/home/alumno/Escritorio/ros2/Proyecto/catkin_ws/src/rosserial/rosserial_tivac/tivac-cmake/cmake/TivaCToolchain.cmake")
